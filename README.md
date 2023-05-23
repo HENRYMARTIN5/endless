8f30da9fbd5db9fcb7ecdd6e432c11f4318d9cb2
HENRYMARTIN5 <62612165+HENRYMARTIN5@users.noreply.github.com>
2023-05-23 16:51:41 -0500

    Add code

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# collector
-A modding tool for Dead Cells
+# Endless
+
+A simple modding tool for Dead Cells that allows modification and/or replacement of the game's files.
+
+## Features
+
+- [x] Unpacking and repacking `res.pak` - the container for all the
+       game's assets.
+- [x] Packaging "bare-minimum" mod packages based on modifications to
+       `res.pak`.
+- [x] Starting either vanilla or modded Dead Cells with specific mods.
+- [ ] More efficient handling of modded launches - binary patching?
+- [ ] Game version compatibility checks
+- [ ] Mod compatability checks
+- [ ] CastleDB diffing/treating different CDB lines as different
+       changes and not having every mod be incompatible if it even
+       touches CDB
+
+## Installation
+
+1. Clone the repo.
+2. `pip install -r requirements.txt`
+3. `python endless.py`
+4. Follow instructions in-app to setup.
+
+## Making and Packaging Mods
+
+1. First, unpack your game files - select the option in the main menu.
+2. Make your changes to the game files - see the below section for things that you can change and add.
+3. Package the mod by selecting the option in the menu.
+4. Profit?
+
+## Overview of Modding
+
+Things that can be done with modding as of now:
+
+- Custom biomes
+- Custom rooms and generation
+- Modification of existing mobs (AI scripting not available)
+- Modification of existing items (Stats, bonuses, spawn rate, etc.)
+
+For more, see the official Dead Cells modding documentation (which this repo heavily depends on, but still differs from slightly) in `<Your Dead Cells Install>/ModTools/ModsDoc.pdf`.
+
+## License
+
+MIT License. See `LICENSE` for more information.
+
+### Licensing Exemptions
+
+- Motion Twin (and the Dead Cells team) is hereby permitted to use this software and the associated documentation in any way they see fit, without any restrictions.
---
+++ endless.py
@@ -0,0 +1,170 @@
+import os
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+from betterlib import logging
+import sys
+import shutil
+
+logger = logging.Logger("./endless.log", "endless")
+root = tk.Tk()
+
+if not os.path.exists(".did_setup"):
+    root.withdraw()
+    messagebox.showinfo("Welcome", "Welcome to Endless - A simple modding utility for Dead Cells. Please select your game install folder.")
+    folder_selected = filedialog.askdirectory()
+    with open(".did_setup", "w") as f:
+        f.write(folder_selected)
+    # show window again
+try:
+    folder_selected
+except NameError:
+    with open(".did_setup", "r") as f:
+        folder_selected = f.read()
+
+logger.info("Selected folder: " + folder_selected)
+
+# draw window
+root.title("Endless")
+root.geometry("250x250")
+root.resizable(False, False)
+
+def check_backup():
+    if os.path.exists(folder_selected + "/backup"):
+        logger.info("Found backup")
+        return True
+    else:
+        messagebox.showwarning("No backup found", "No backup found. Please backup your game files first, as shown in the installation guide.")
+        logger.warn("No backup found")
+        return False
+
+def unpack(show_messagebox=True, unpack_filename="res.pak", unpack_to="UnpackedFiles"):
+    check_backup()
+    logger.info("Unpacking game files... (this may take a while)")
+    origDir = os.getcwd()
+    os.chdir(folder_selected)
+    os.system("ModTools\PAKTool.exe -Expand -outdir " + unpack_to + " -refpak " + unpack_filename)
+    os.chdir(origDir)
+    logger.info("Unpacked game files!")
+    if show_messagebox:
+        messagebox.showinfo("Done", "Unpacked game files!")
+
+def repack():
+    check_backup()
+    logger.info("Repacking game files... (this may take a while)")
+    origDir = os.getcwd()
+    os.chdir(folder_selected)
+    os.system("ModTools\PAKTool.exe -Collapse -indir UnpackedFiles -outpak res.pak")
+    logger.info("Repacked files, deleting unpacked files...")
+    shutil.rmtree("UnpackedFiles")
+    os.chdir(origDir)
+    logger.info("Repacked game files!")
+    messagebox.showinfo("Done", "Repacked game files!")
+
+def fixPath(path):
+    fixed = ""
+    j = 0
+    for i in path:
+        if j <= len(fixed):
+            fixed += i + "/"
+            continue
+        fixed += i
+    return fixed
+
+def package():
+    check_backup()
+    logger.info("Unpacking game files for diff... (this may take a while)")
+    origDir = os.getcwd()
+    os.chdir(folder_selected)
+    os.system("ModTools\PAKTool.exe -Expand -outdir UnpackedFiles2 -refpak backup/res.pak")
+    logger.info("Unpacked vanilla files.")
+    logger.info("Diffing files...")
+    os.system("diff -rq UnpackedFiles UnpackedFiles2 > diff.txt")
+    logger.info("Deleting unpacked files...")
+    shutil.rmtree("UnpackedFiles2")
+    os.makedirs("ModFiles", exist_ok=True)
+    logger.info("Copying changed files...")
+    changed = []
+    with open(folder_selected + "/diff.txt", "r") as f:
+        for diff in f.readlines():
+            if diff.strip().endswith("differ"):
+                changed.append(fixPath(diff.split("Files ")[1].split(" and ")[0].split("/")[1:]))
+    logger.info("Changed files: " + str(changed))
+    for file in changed:
+        fixedFile = file[:-1]
+        logger.debug("copying " + fixedFile)
+        # make directory if it doesn't exist
+        os.makedirs("./ModFiles/" + "/".join(fixedFile.split("/")[:-1]), exist_ok=True)
+        shutil.copyfile(folder_selected + "/UnpackedFiles/" + fixedFile, "./ModFiles/" + fixedFile)
+    logger.info("Copied changed files to ModFiles.")
+    logger.info("Packing mod...")
+    origDir = os.getcwd()
+    os.chdir(folder_selected)
+    os.system("ModTools\PAKTool.exe -Collapse -indir " + origDir + "\ModFiles -outpak " + origDir + "\mod.pak")
+    logger.info("Packed mod!")
+    os.chdir(origDir) # just in case
+    shutil.rmtree("ModFiles")
+
+def start_modded():
+    logger.info("Prompting user to choose mods...")
+    messagebox.showinfo("Choose mods", "Please choose the mods you want to use.")
+    mod_selected = filedialog.askopenfilenames(defaultextension=".pak", filetypes=[("Pak files", "*.pak")])
+    logger.info("Selected mods: " + str(mod_selected))
+    logger.info("Checking unpacked files...")
+    if not os.path.exists(folder_selected + "/UnpackedFiles"):
+        logger.info("Unpacked files not found, unpacking...")
+        unpack(show_messagebox=False, unpack_filename="backup/res.pak")
+    logger.info("Unpacked vanilla pak, unpacking mods...")
+    i = 0
+    for mod in mod_selected:
+        unpack(show_messagebox=False, unpack_filename=mod, unpack_to="mod" + str(i))
+        i += 1
+    logger.info("Overwriting vanilla files with modded files...")
+    j = 0
+    for mod in mod_selected:
+        try:
+            shutil.copytree("mod" + str(i), "UnpackedFiles")
+        except:
+            logger.error("Failed to copy mod: " + mod + ". Will continue anyway.")
+        j += 1
+    logger.info("Packing modded files...")
+    repack()
+    logger.info("Deleting unpacked mods...")
+    i = 0
+    for mod in mod_selected:
+        try:
+            shutil.rmtree("mod" + str(i))
+        except:
+            logger.error("Failed to delete mod: " + mod + ". Will continue anyway.")
+        i += 1
+    logger.info("Starting game (modded pak)...")
+    origDir = os.getcwd()
+    os.chdir(folder_selected)
+    os.system("start deadcells.exe")
+    os.chdir(origDir)
+
+def start_vanilla():
+    logger.info("Starting game (vanilla pak)...")
+    origDir = os.getcwd()
+    os.chdir(folder_selected)
+    shutil.copyfile("backup/res.pak", "res.pak")
+    os.system("start deadcells.exe")
+    os.chdir(origDir)
+    
+# header
+tk.Label(root, text="Endless", font=("Arial", 20)).grid(row=0, column=5)
+tk.Label(root, text="A simple modding utility for Dead Cells", font=("Arial", 10)).grid(row=1, column=5)
+
+buttons = {
+    "Unpack game files": unpack,
+    "Repack game files": repack,
+    "Package mod": package,
+    "Start game (Modded)": start_modded,
+    "Start game (vanilla)": start_vanilla,
+    "Exit": sys.exit
+}
+
+for i, (text, command) in enumerate(buttons.items()):
+    tk.Button(root, text=text, command=command).grid(row=(i*2)+4, column=5)
+
+root.mainloop()
